591b565b05b92d3b24e9b6e7ac07874896560afe
Alex <ato31ato@gmail.com>
2024-08-17 22:05:10 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  build: {
-    outDir: "./docs",
-  },
+  base: "/the-wild-oasis",
 });
